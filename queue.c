89a8a6838e69b5ca3b903a3e8ca557db569f11ea
Eddie Lin <eddielin0926@gmail.com>
2023-02-20 02:33:50 +0800

    Implement `q_reverse` to reverse the queue
    The `q_reverse` function reverses the order of the elements in the
    queue. If the queue is empty, it does nothing.

--- queue.c
+++ queue.c
@@ -155,7 +155,18 @@ void q_swap(struct list_head *head)
 }
 
 /* Reverse elements in queue */
-void q_reverse(struct list_head *head) {}
+void q_reverse(struct list_head *head)
+{
+    if (!head || list_empty(head) || list_is_singular(head))
+        return;
+    struct list_head *node, *safe;
+    list_for_each_safe (node, safe, head) {
+        node->next = node->prev;
+        node->prev = safe;
+    }
+    node->next = node->prev;
+    node->prev = safe;
+}
 
 /* Reverse the nodes of the list k at a time */
 void q_reverseK(struct list_head *head, int k)
