51acaeea8afb5502a843a215eea49443dfef9e0b
Eddie Lin <eddielin0926@gmail.com>
2023-02-20 01:51:29 +0800

    Implement `q_swap` to swap elements of the queue
    The `q_swap` function swaps every two element of the queue. If the queue
    has zero or one element, the function does nothing.

--- queue.c
+++ queue.c
@@ -147,6 +147,11 @@ bool q_delete_dup(struct list_head *head)
 void q_swap(struct list_head *head)
 {
     // https://leetcode.com/problems/swap-nodes-in-pairs/
+    if (!head || list_empty(head) || list_is_singular(head))
+        return;
+    struct list_head *node;
+    list_for_each (node, head)
+        list_move(node, node->next);
 }
 
 /* Reverse elements in queue */
