5056939ac8babacbcd427ce60706b9d955e00a88
Eddie Lin <eddielin0926@gmail.com>
2023-02-20 00:28:00 +0800

    Implement `q_remove_tail` for head remove
    The `q_remove_head` function removes the last element of the queue and
    returns it. In addition, The function does not free the memory of the
    removed element. If the queue is empty, the function returns `NULL`. If
    the `sp` parameter is not `NULL`, the function creates a copy of the
    removed element and stores it in the memory pointed by `sp`.

--- queue.c
+++ queue.c
@@ -85,7 +85,13 @@ element_t *q_remove_head(struct list_head *head, char *sp, size_t bufsize)
 /* Remove an element from tail of queue */
 element_t *q_remove_tail(struct list_head *head, char *sp, size_t bufsize)
 {
-    return NULL;
+    if (!head || list_empty(head))
+        return NULL;
+    element_t *el = container_of(head->prev, element_t, list);
+    if (sp)
+        strncpy(sp, el->value, bufsize);
+    list_del(&el->list);
+    return el;
 }
 
 /* Return number of elements in queue */
